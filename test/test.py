10ffb14552a4870b04e1d2ce9bf16f9fc9eaf23f
Alienfalcon <34289474+Alienfalcon@users.noreply.github.com>
2026-02-26 11:06:32 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -15,15 +15,20 @@ async def test_project(dut):
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
-
+    #create output variable
+    output = cocotb.types.LogicArray.from_unsigned(0x00, 8)
     # Reset
     
     dut._log.info("Reset")
+    #Turn on the module
     dut.ena.value = 1
+    #Set inputs to module
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+    #Reset the module
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
+    #Turn off reset
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
@@ -32,27 +37,27 @@ async def test_project(dut):
     for i in range(0,1000):
         A = cocotb.types.LogicArray.from_unsigned(0x00, 8)
         B = cocotb.types.LogicArray.from_unsigned(0x00, 8)
-        
+
+        #randomly create 2 logic arrays of 4 bits to concatenate together
         for j in range(0,4):
             A = cocotb.types.LogicArray.from_unsigned((random.random()>0.5)<<(j+4),8) | A;
             B = cocotb.types.LogicArray.from_unsigned((random.random()>0.5)<<j,8) | B;
         u_in = A | B;
-        dut._log.info("U_IN")
-        dut._log.info(u_in)
+        #cast logic arrays to ints and multiply them
         A_int = int(A) >> 4
         B_int = int(B)
         P_int = A_int * B_int
         P = cocotb.types.LogicArray.from_unsigned(0x00, 8)
         P.value = P_int
-        
+
+        #send in new value to the array
         dut.ui_in.value = u_in
-        output = cocotb.types.LogicArray.from_unsigned(0x00, 8)
+        #wait for 3 clk cycles
         await ClockCycles(dut.clk, 3)
+        #record the output
         output.value = dut.uo_out.value
-        dut._log.info("output clk3")
-        dut._log.info(dut.uo_out.value)      
-        
         correct = (P.value == output.value) + correct
+        #test if output matches expected value
         assert (P.value == output.value)
 
     fin_out_str = f"{correct} out of 1000 tests have succeeded"
